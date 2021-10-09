ecb2c250f1bc25123125ca3648d41c33d1faa226
jessazam <jess.azamantilla@gmail.com>
2021-10-09 16:40:09 -0500

    cambios contacto

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -1,5 +1,20 @@
 <template>
-    <h2>Contacto</h2>
+    <div class="mx-auto my-15">
+
+        <v-row justify="space-around" class="mt-5 mb-8">
+            <a class="text-decoration-none text-h6 grey--text text--darken-3" href="https://wa.me/573014016385?text=Quiero%20comprar%20un%20producto"><v-icon large color="green darken-2"> mdi-whatsapp</v-icon><span class="mx-1">Escríbenos</span></a>
+
+            <a class="text-decoration-none text-h6 grey--text text--darken-3" href="https://www.facebook.com/robeal1693"><v-icon large color="blue darken-2">mdi-facebook</v-icon><span class="mx-1">Visítanos</span></a>
+
+            <a class="text-decoration-none text-h6 grey--text text--darken-3" href="tel:+3003707389"><v-icon large color="purple darken-2">mdi-phone-in-talk</v-icon><span class="mx-1">Llámanos</span></a>
+        </v-row>
+        <v-row justify="space-around" class="text-h5 grey--text text--darken-3 my-8">
+            <div class="my-5">
+                También puedes visitarnos en nuestra tienda física ubicada en San Gíl, Santander
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.7573334915896!2d-73.13098978596364!3d6.5522910246475945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e69c72f344146e3%3A0x612b966d253611d2!2sCl.%207%20%239-29%2C%20San%20Gil%2C%20Santander!5e0!3m2!1ses!2sco!4v1633815083948!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </v-row>
+    </div>
 </template>
 
 <script>
@@ -9,5 +24,7 @@ export default {
 </script>
 
 <style>
-
+    a{
+        
+    }
 </style>
